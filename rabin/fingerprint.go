d347407d49703dc37931733cb99595cb6ada2fcb
Emil Georgiev <emogeorgiev88@gmail.com>
2023-02-05 11:02:21 +0200

    feat: refactor implementation of the rabin fingerprint

--- rabin/fingerprint.go
+++ rabin/fingerprint.go
@@ -1,17 +1,20 @@
 package rabin
 
 const (
-	defaultModulus = 8191
-	multiplier     = 257
+	// 2^13 irreducible polynomial
+	defaultModulus      = 8191
+	multiplier          = 257
+	numberOfBitsPerByte = 8
 )
 
 type FingerprintHash interface {
 	Value() uint64
+	Next(byte) uint64
 }
 
 type rabinFingerprintHash struct {
-	value           uint64
-	characterValues []uint64
+	value                    uint64
+	polynomialsOverGF2Values []uint64
 }
 
 func (r rabinFingerprintHash) Value() uint64 {
@@ -29,42 +32,42 @@ func Pow(a uint64, b int) uint64 {
 	return n
 }
 
-func getNumberOfBytes(i int32) int {
-	switch true {
-	case i <= 127:
-		return 1
-	case i <= 2047:
-		return 2
-	case i <= 65535:
-		return 3
-	case i <= 1114111:
-		return 4
-	}
-
-	return 0
-}
-
-func NewFingerprintHash(str string) FingerprintHash {
+func NewFingerprintHash(bytes []byte) FingerprintHash {
 	var rfh rabinFingerprintHash
 	var h uint64
-	degreeOfPolynomial := len(str) * 8
-	for _, b := range str {
+	degreeOfPolynomial := len(bytes) * numberOfBitsPerByte
+	for _, b := range bytes {
 		var polynomialOverGF2 uint64
-		numberOfBytes := getNumberOfBytes(b)
-		for j := 8*numberOfBytes - 1; j >= 0; j-- {
+		for j := 7; j >= 0; j-- {
 			// polynomials over GF(2)
 			mask := int32(1 << uint(j))
 			degreeOfPolynomial -= 1
-			p := Pow(multiplier, degreeOfPolynomial)
-			bit := b & mask
+			term := Pow(multiplier, degreeOfPolynomial)
+			bit := int32(b) & mask
 			if bit == 0 {
 				continue
 			}
-			polynomialOverGF2 += p
+			polynomialOverGF2 += term
 		}
-		rfh.characterValues = append(rfh.characterValues, polynomialOverGF2)
+		rfh.polynomialsOverGF2Values = append(rfh.polynomialsOverGF2Values, polynomialOverGF2)
 		h += polynomialOverGF2
 	}
 	rfh.value = h % defaultModulus
 	return rfh
+}
+
+func (rfh rabinFingerprintHash) Next(b byte) uint64 {
+	var polynomialOverGF2 uint64
+	for j := 7; j >= 0; j-- {
+		mask := int32(1 << uint(j))
+		term := Pow(multiplier, j)
+		bit := int32(b) & mask
+		if bit == 0 {
+			continue
+		}
+		polynomialOverGF2 += term
+	}
+	rfh.value = (((rfh.value+defaultModulus-rfh.polynomialsOverGF2Values[0]%defaultModulus)*Pow(multiplier, 8))%defaultModulus + polynomialOverGF2) % defaultModulus
+	rfh.polynomialsOverGF2Values = append(rfh.polynomialsOverGF2Values[1:], polynomialOverGF2)
+	return rfh.value
 }
